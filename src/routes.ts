e7ebf6988887751f37b04d21613edeb6c9be0818
andreferraz20 <luizlopesferraz22@gmail.com>
2021-12-29 19:18:39 -0300

    CreateDelivery

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { CreateDeliverymanController } from "./modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 const routes = Router();
 
@@ -9,6 +10,7 @@ const createClientController = new CreateClientController();
 const authenticateClientController = new AuthenticateClientController();
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const createDeliverymanController = new CreateDeliverymanController();
+const createDeliveryController = new CreateDeliveryController();
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 
@@ -17,5 +19,7 @@ routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handl
 routes.post("/client/", createClientController.handle);
 
 routes.post("/deliveryman", createDeliverymanController.handle);
+
+routes.post("/delivery", createDeliveryController.handle);
 
 export { routes }; 
