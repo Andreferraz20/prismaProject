953438c5112f63f3a318b86fd3ceb84bce7c105a
andreferraz20 <luizlopesferraz22@gmail.com>
2021-12-30 13:34:12 -0300

    Find all Deliveries for an user

---
+++ src/modules/clients/useCases/deliveries/FindAllDeliveriesUseCase.ts
@@ -0,0 +1,22 @@
+import { prisma } from "../../../../database/prismaClient";
+
+export class FindAllDeliveriesUseCase {
+    async execute(id_client: string) {
+        /*const deliveries = await prisma.deliveries.findMany({
+            where: {
+                id_client
+            }
+        })*/
+
+        const deliveries = await prisma.clients.findMany({
+            where: {
+                id: id_client
+            },
+            include: {
+                deliveries: true
+            },
+        })
+
+        return deliveries;
+    }
+}
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { ensureAuthenticateDeliveryman } from "./middlewares/ensureAuthenticateD
 import { AuthenticateClientController } from "./modules/account/authenticateClient/AuthenticateClientController";
 import { AuthenticateDeliverymanController } from "./modules/account/authenticateDeliveryman/AuthenticateDeliverymanController";
 import { CreateClientController } from "./modules/clients/useCases/createClient/CreateClientController";
+import { FindAllDeliveriesController } from "./modules/clients/useCases/deliveries/FindAllDeliveriesController";
 import { CreateDeliveryController } from "./modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 import { FindAllAvailableController } from "./modules/deliveries/useCases/findAllAvailable/FindAllAvailableController";
 import { UpdateDeliverymanController } from "./modules/deliveries/useCases/updateDeliveryman/UpdateDeliverymanController";
@@ -18,6 +19,7 @@ const createDeliverymanController = new CreateDeliverymanController();
 const createDeliveryController = new CreateDeliveryController();
 const findAllAvailableController = new FindAllAvailableController();
 const updateDeliverymanController = new UpdateDeliverymanController();
+const findAllDeliveriesClient = new FindAllDeliveriesController()
 
 routes.post("/client/authenticate", authenticateClientController.handle);
 
@@ -31,6 +33,8 @@ routes.post("/delivery", ensureAuthenticateClient, createDeliveryController.hand
 
 routes.get("/delivery/available", ensureAuthenticateDeliveryman, findAllAvailableController.handle);
 
-routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle)
+routes.put("/delivery/updateDeliveryman/:id", ensureAuthenticateDeliveryman, updateDeliverymanController.handle);
+
+routes.get("/client/deliveries", findAllDeliveriesClient.handle);
 
 export { routes }; 
